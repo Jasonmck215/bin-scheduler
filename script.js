e156419b8651593f2f05d2f82f0cc33a2daa198e
Jasonmck215 <145149148+Jasonmck215@users.noreply.github.com>
2025-02-12 20:44:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,22 @@ document.addEventListener('DOMContentLoaded', function () {
         // Sort dates by date
         allDates.sort((a, b) => new Date(a.date.split('/').reverse().join('-')) - new Date(b.date.split('/').reverse().join('-')));
 
-        return allDates;
+        const groupedDates = allDates.reduce((acc, { date, bin }) => {
+            if (!acc[date]) {
+                acc[date] = [];
+            }
+            acc[date].push(bin);
+            return acc;
+        }, {});
+
+        // Convert groupedDates to the desired format
+        const sortedDates = Object.keys(groupedDates).sort((a, b) => new Date(a.split('/').reverse().join('-')) - new Date(b.split('/').reverse().join('-')));
+        const result = sortedDates.reduce((acc, date) => {
+            acc[date] = groupedDates[date].join(', ');
+            return acc;
+        }, {});
+
+        return result;
     }
 
     function formatDate(date) {
